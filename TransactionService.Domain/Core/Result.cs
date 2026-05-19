ca452ceb25b97e3080ef365a0b00c522eaa93da1
Dalton DeMaria <vvd@Dalton.local>
2026-05-19 17:01:51 -0500

    000 - more safety around error list in result

--- TransactionService.Domain/Core/Result.cs
+++ TransactionService.Domain/Core/Result.cs
@@ -9,7 +9,7 @@ public class Result<T>
     
     public bool Success => Errors.Count == 0;
     public T? GetValue() => Success ? Value : default;
-    public List<string> GetErrors() => Errors;
+    public IReadOnlyList<string> GetErrors() => Errors;
 
     public static Result<T> OK(T data) => new() { Value = data };
     public static Result<T> Fail(params string[] errors) => new() { Errors = [..errors] };
